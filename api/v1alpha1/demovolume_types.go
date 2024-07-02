253577052038a074e3e51174c1568d08c719a223
Lepcha, Nikil (NBCUniversal) <Nikil.Lepcha@nbcuni.com>
2024-07-02 12:38:42 +0530

    updated code

--- api/v1alpha1/demovolume_types.go
+++ api/v1alpha1/demovolume_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -29,13 +13,15 @@ type DemoVolumeSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of DemoVolume. Edit demovolume_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Name string `json:"name,omitempty"`
+	Size int    `json:"size,omitempty"`
 }
 
 // DemoVolumeStatus defines the observed state of DemoVolume
 type DemoVolumeStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Name string `json:"name,omitempty"`
 }
 
 // +kubebuilder:object:root=true
